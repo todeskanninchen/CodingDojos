ac01747c05768fce597cde8592e2d7a4a0ba0518
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-02-16 15:57:07 +0100

    FizzBuzz

--- FizzBuzz2016_02_16/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
+++ FizzBuzz2016_02_16/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
@@ -44,6 +44,12 @@
             Should_Convert_Number_To_ExpectedOutput(5, "Buzz");
         }
 
+        [TestMethod]
+        public void CanConvertFifteen()
+        {
+            Should_Convert_Number_To_ExpectedOutput(15, "FizzBuzz");
+        }
+
         private void Should_Convert_Number_To_ExpectedOutput(int numberToConvert, string expected)
         {
             string result = _sut.Convert(numberToConvert);
--- FizzBuzz2016_02_16/FizzBuzz/FizzBuzzConverter.cs
+++ FizzBuzz2016_02_16/FizzBuzz/FizzBuzzConverter.cs
@@ -2,11 +2,20 @@
 {
     public class FizzBuzzConverter
     {
+        private const int BuzzNumber = 5;
+        private const int FizzNumber = 3;
+        private const int FizzBuzzNumber = FizzNumber * BuzzNumber;
+
         private const string Buzz = "Buzz";
         private const string Fizz = "Fizz";
 
         public string Convert(int number)
         {
+            if (IsFizzBuzz(number))
+            {
+                return Fizz + Buzz;
+            }
+
             if (IsFizz(number))
             { 
                 return Fizz;
@@ -20,14 +29,19 @@
             return number.ToString();
         }
 
+        private bool IsFizzBuzz(int number)
+        {
+            return number % FizzBuzzNumber == 0;
+        }
+
         private bool IsBuzz(int number)
         {
-            return number % 5 == 0;
+            return number % BuzzNumber == 0;
         }
 
         private bool IsFizz(int number)
         {
-            return number % 3 == 0;
+            return number % FizzNumber == 0;
         }
     }
 }
