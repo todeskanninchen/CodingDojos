50147349c10e511879a0bb932b53a964578ba72f
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-02-16 14:25:15 +0100

    Create Project structure and first test class

---
+++ FizzBuzz2016_02_16/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace FizzBuzz.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FizzBuzzGeneratorTests
+    {
+        [TestMethod]
+        public void CanCreateInstance()
+        {
+            var fbz = new FizzBuzzGenerator();
+        }
+    }
+}
