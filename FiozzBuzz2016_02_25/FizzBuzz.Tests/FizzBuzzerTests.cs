7068a6fdb2597feaae5a17a18774872964d81531
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-02-25 15:13:59 +0100

    added FizzBuzzzzzzzzz for intervals

--- FiozzBuzz2016_02_25/FizzBuzz.Tests/FizzBuzzerTests.cs
+++ FiozzBuzz2016_02_25/FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -13,32 +13,18 @@
         {
             _fizzBuzzer = new FizzBuzzer();
         }
-        [TestMethod]
-        public void FirstElementOfResultShouldBe1()
-        {
-            var result = _fizzBuzzer.FizzBuzzFrom1To16();
-            result[0].Should().Be(1.ToString());
-        }
-
-        [TestMethod]
-        public void ShouldHave16Elements()
-        {
-            var result = _fizzBuzzer.FizzBuzzFrom1To16();
-            result.Length.Should().Be(16);
-        }
-
+        
         [TestMethod]
         public void FizzBuzzInterval_from_1_to_1_should_return_1()
         {
             var result = _fizzBuzzer.FizzBuzzInterval(1, 1);
-            result.Length.Should().Be(1);
-            result[0].Should().Be("1");
+            result.Should().BeEquivalentTo("1");
         }
 
         [TestMethod]
         public void FizzBuzzFrom1To16()
         {
-            var result = _fizzBuzzer.FizzBuzzFrom1To16();
+            var result = _fizzBuzzer.FizzBuzzInterval(1, 16);
             result.Should().BeEquivalentTo("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16");
         }
 
@@ -49,5 +35,11 @@
             result.Should().BeEquivalentTo("Fizz", "502", "503", "Fizz", "Buzz");
         }
 
+        [TestMethod]
+        public void FizzBuzzInterval_from_1500_to_1500_should_return_FizzBuzz()
+        {
+            var result = _fizzBuzzer.FizzBuzzInterval(1500, 1500);
+            result.Should().BeEquivalentTo("FizzBuzz");
+        }
     }
 }
--- FiozzBuzz2016_02_25/FizzBuzz/FizzBuzzer.cs
+++ FiozzBuzz2016_02_25/FizzBuzz/FizzBuzzer.cs
@@ -4,34 +4,16 @@
 
     public class FizzBuzzer
     {
-        public string[] FizzBuzzFrom1To16()
-        {
-            var result = new string[16];
-            result[0] = "1";
-            result[1] = "2";
-            result[2] = "Fizz";
-            result[3] = "4";
-            result[4] = "Buzz";
-            result[5] = "Fizz";
-            result[6] = "7";
-            result[7] = "8";
-            result[8] = "Fizz";
-            result[9] = "Buzz";
-            result[10] = "11";
-            result[11] = "Fizz";
-            result[12] = "13";
-            result[13] = "14";
-            result[14] = "FizzBuzz";
-            result[15] = "16";
-            return result;
-        }
-
         public string[] FizzBuzzInterval(int start, int end)
         {
             var result = new List<string>();
             for (int i = start; i <= end; i++)
             {
-                if (IsFizz(i))
+                if (IsFizz(i) && IsBuzz(i))
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (IsFizz(i))
                 {
                     result.Add("Fizz");
                 }
