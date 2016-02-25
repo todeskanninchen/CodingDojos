d937e245bdea04328b9deeb1af829064750c53c8
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-02-25 14:04:20 +0100

    first element should be 1 should have 100 elements

--- FiozzBuzz2016_02_25/FizzBuzz.Tests/FizzBuzzerTests.cs
+++ FiozzBuzz2016_02_25/FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -6,12 +6,25 @@
     [TestClass]
     public class FizzBuzzerTests
     {
-        [TestMethod]
-        public void Can_create_FizzBuzzer()
-        {
-            var fizzBuzzer = new FizzBuzzer();
+        private FizzBuzzer _fizzBuzzer;
 
-            fizzBuzzer.Should().NotBeNull();
+        [TestInitialize]
+        public void Setup()
+        {
+            _fizzBuzzer = new FizzBuzzer();
+        }
+        [TestMethod]
+        public void FirstElementOfResultShouldBe1()
+        {
+            var result = _fizzBuzzer.FizzBuzzFrom1To100();
+            result[0].Should().Be(1.ToString());
+        }
+
+        [TestMethod]
+        public void ShouldHave100Elements()
+        {
+            var result = _fizzBuzzer.FizzBuzzFrom1To100();
+            result.Length.Should().Be(100);
         }
     }
 }
--- FiozzBuzz2016_02_25/FizzBuzz/FizzBuzzer.cs
+++ FiozzBuzz2016_02_25/FizzBuzz/FizzBuzzer.cs
@@ -2,5 +2,11 @@
 {
     public class FizzBuzzer
     {
+        public string[] FizzBuzzFrom1To100()
+        {
+            var result = new string[100];
+            result[0] = "1";
+            return result;
+        }
     }
 }
