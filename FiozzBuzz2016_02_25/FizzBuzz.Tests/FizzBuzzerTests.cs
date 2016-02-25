9f4b544221d345ace4ed0c31cca7790cd631ce89
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-02-25 13:50:56 +0100

    fizzbuzz init stuff

---
+++ FiozzBuzz2016_02_25/FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,17 @@
+﻿namespace FizzBuzz.Tests
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FizzBuzzerTests
+    {
+        [TestMethod]
+        public void Can_create_FizzBuzzer()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            fizzBuzzer.Should().NotBeNull();
+        }
+    }
+}
