911bed00c4c0d94e6a1883346d37ff66a38150a1
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-03-09 15:01:37 +0100

    can split a line of text by semicolon

--- CSV_09_03_2016/CsvViewer.Tests.Uni/CsvReaderTests.cs
+++ CSV_09_03_2016/CsvViewer.Tests.Uni/CsvReaderTests.cs
@@ -9,10 +9,21 @@ namespace CsvViewer.Tests
     public class CsvReaderTests
     {
         [TestMethod]
-        public void Output_Nothing_When_Input_Is_Empty()
+        public void Nothing_To_Read_When_Input_Is_Empty()
         {
             var reader = new CsvReader(string.Empty);
             reader.Read().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Output_Should_Be_Splitted_with_One_Line_Of_Input()
+        {
+            var input = "abc;def;ghi";
+            var reader = new CsvReader(input);
+
+            reader.Read().Should().BeTrue();
+
+            reader.GetCurrent().Should().BeEquivalentTo("abc", "def", "ghi");
         }
     }
 }
--- CSV_09_03_2016/csvviewer/CsvReader.cs
+++ CSV_09_03_2016/csvviewer/CsvReader.cs
@@ -2,13 +2,21 @@ namespace CsvViewer
 {
     public class CsvReader
     {
+        private readonly string _fileContent;
+
         public CsvReader(string fileContent)
         {
+            _fileContent = fileContent;
         }
 
         public bool Read()
         {
-            return false;
+            return !string.IsNullOrEmpty(_fileContent);
+        }
+
+        public string[] GetCurrent()
+        {
+            return _fileContent.Split(';');
         }
     }
 }
