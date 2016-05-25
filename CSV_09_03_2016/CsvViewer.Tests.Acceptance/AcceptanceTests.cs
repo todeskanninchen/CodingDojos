526130de321fa2b63fb5c14c24946f55e975addc
Schulze, Thomas <Thomas.Schulze@comparex.de>
2016-05-25 08:25:32 +0200

    pseudo implementation (not done) for process method

--- CSV_09_03_2016/CsvViewer.Tests.Acceptance/AcceptanceTests.cs
+++ CSV_09_03_2016/CsvViewer.Tests.Acceptance/AcceptanceTests.cs
@@ -1,20 +1,42 @@
 ﻿namespace CsvViewer.Tests.Acceptance
 {
+    using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using FluentAssertions;
 
     [TestClass]
     public class AcceptanceTests
     {
+        private const string TestFilesDirectory = "_TestData";
+
         [TestMethod]
-        [DeploymentItem("_TestData/emtpyFile.csv")]
+        [DeploymentItem(TestFilesDirectory)]
         public void Emtpy_File_Should_Display_Nothing()
         {
+            const string emptyFile = "emptyFile.csv";
+
             var csvProcessor = new CsvProcessor();
-            csvProcessor.Process("emptyFile.csv");
+            csvProcessor.Process(emptyFile);
 
             var result = csvProcessor.GetResult();
             result.Should().BeEmpty();
         }
+
+        [TestMethod]
+        [Ignore]
+        [DeploymentItem(TestFilesDirectory)]
+        public void Person_File_Should_Display_Headerline_Correctly()
+        {
+            var csvProcessor = new CsvProcessor();
+            csvProcessor.Process("singleLineFile.csv");
+
+            var result = csvProcessor.GetResult();
+            result.Should().Be(
+                "|City|Age|Name|"
+                + Environment.NewLine
+                + "+----+---+----+");
+        }
+
+
     }
 }
--- CSV_09_03_2016/csvviewer/CsvProcessor.cs
+++ CSV_09_03_2016/csvviewer/CsvProcessor.cs
@@ -1,14 +1,23 @@
 ﻿namespace CsvViewer
 {
+    using System.IO;
+
     public class CsvProcessor
     {
-        public void Process(string emptyfileCsv)
+        private string result = string.Empty;
+        public void Process(string inputFile)
         {
+            string fileContent = File.ReadAllText(inputFile);
+            CsvReader reader = new CsvReader(fileContent);
+            reader.Read();
+            // var headerLineValues = reader.GetCurrent();
+            //CsvFormatter formatter = new CsvFormatter();
+            //result = formatter.FormatLine(headerLineValues);
         }
 
         public string GetResult()
         {
-            return string.Empty;
+            return result;
         }
     }
 }
