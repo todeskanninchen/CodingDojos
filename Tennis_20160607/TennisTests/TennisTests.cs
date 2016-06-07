9172f7acbeb9847e2ad9818506372b4e44040d1c
Flöring, Stefan <Stefan.Floering@partner.comparex.com>
2016-06-07 14:07:44 +0200

    init stuff and first test

---
+++ Tennis_20160607/TennisTests/TennisTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisTests
+{
+    using FluentAssertions;
+
+    using Tennis_20160607;
+
+    [TestClass]
+    public class TennisTests
+    {
+        [TestMethod]
+        public void PlayerAScoresOnce()
+        {
+            var tennis = new Tennis();
+            tennis.CalculateScore().Should().Be("0:0");
+        }
+    }
+}
---
+++ Tennis_20160607_2/Tennis_20160607_2/Tennis.cs
@@ -0,0 +1,10 @@
+﻿namespace Tennis_20160607
+{
+    public class Tennis
+    {
+        public string CalculateScore()
+        {
+            return "0:0";
+        }
+    }
+}
